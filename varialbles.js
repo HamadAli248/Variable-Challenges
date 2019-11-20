8ccf6f8f61d19754129ab7c0459a6b458da9719f
Hamad Ali <hammadali248@gmail.com>
2019-11-20 17:01:56 +0000

    Variable challenges

---
+++ varialbles.js
@@ -0,0 +1,34 @@
+/*JS Variables
+Challenge 1:
+Create a 9 variables space1, space2... space9
+
+Challenge 2:
+Assign either the value ‘x’, ‘o’, ‘ ‘ to each of these variable
+
+Challenge 3:
+Insert the variables into the board using the ${varName} syntax and make your board look like the one displayed
+*/
+
+let space1 = "x" 
+let space2 = "0" 
+let space3 = "" 
+let space4 = "x" 
+let space5 = "0" 
+let space6 = "" 
+let space7 = "x" 
+let space8 = "" 
+let space9 = "0"
+
+console.log("  |   |")
+console.log(`${space1} | ${space2}  |${space3}`)
+console.log("  |    |  ")
+console.log(" ---------  ")
+console.log("  |    |  ")
+console.log(`${space4} |${space5}   |${space6} `)
+console.log("  |    |  ")
+console.log(" ---------  ")
+console.log("  |    |  ")
+console.log(`${space7} |${space8}   |${space9}  `)
+console.log("  |    |  ")
+
+
